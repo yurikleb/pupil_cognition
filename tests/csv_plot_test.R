9e3198804d8e7d73ad209342e907cc896f00a1da
Unknown <yurikleb@gmail.com>
2018-04-19 21:45:49 +0300

    Aded interactive plots in R

--- tests/csv_plot_test.R
+++ tests/csv_plot_test.R
@@ -1,6 +1,7 @@
 ## Packages
 library(data.table)
 library(ggplot2)
+library(plotly)
 
 ## List the files in the folder <---- INSERT THE CORRECT PATH OF THE FILES!
 (f <- list.files("~/Desktop/Experiment_Data/Christian", full.names = TRUE, pattern = "\\.csv"))
@@ -16,6 +17,9 @@ DT <- Reduce(function(...) merge(..., by = "V1", all = TRUE), l)
 DT_long <- melt(DT, id = "V1")
 
 ## Plot
-ggplot(DT_long, aes(V1, value, color = variable)) + 
+myPlot <- 
+  ggplot(DT_long, aes(V1, value, color = variable)) + 
   geom_point() + xlim(400,3500) + ylim(0,17)
 
+## Create Interactive Plot
+ggplotly(myPlot)
